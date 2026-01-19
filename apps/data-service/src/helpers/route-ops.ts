81f861ffd5e22dfa22643e689938618ce4e83471
Usman S. <ui.sabuwala@gmail.com>
2026-01-19 15:53:24 +0000

    added kv for performance

--- apps/data-service/src/helpers/route-ops.ts
+++ apps/data-service/src/helpers/route-ops.ts
@@ -1,4 +1,41 @@
-import type { LinkSchemaType } from '@repo/data-ops/zod-schema/links';
+import { getLink } from '@repo/data-ops/queries/links';
+import { linkSchema, type LinkSchemaType } from '@repo/data-ops/zod-schema/links';
+
+const TTL_TIME = 60 * 60 * 24;
+
+export async function getLinkInfoFromKv(env: Env, id: string) {
+	const linkInfo = await env.CACHE.get(id);
+	if (!linkInfo) return null;
+
+	try {
+		const parsedLinkInfo = JSON.parse(linkInfo);
+		return linkSchema.parse(parsedLinkInfo);
+	} catch {
+		return null;
+	}
+}
+
+export async function saveLinkInfoToKv(env: Env, id: string, linkInfo: LinkSchemaType) {
+	try {
+		await env.CACHE.put(id, JSON.stringify(linkInfo), {
+			expirationTtl: TTL_TIME,
+		});
+	} catch (error) {
+		console.error('Error saving link info to KV', error);
+	}
+}
+
+export async function getRoutingDestination(env: Env, id: string) {
+	const linkInfo = await getLinkInfoFromKv(env, id);
+	if (linkInfo) return linkInfo;
+
+	const linkInfoFromDb = await getLink(id);
+	if (!linkInfoFromDb) return null;
+
+	await saveLinkInfoToKv(env, id, linkInfoFromDb);
+
+	return linkInfoFromDb;
+}
 
 export function getDestinationForCountry(linkInfo: LinkSchemaType, countryCode?: string) {
 	if (!countryCode) {
