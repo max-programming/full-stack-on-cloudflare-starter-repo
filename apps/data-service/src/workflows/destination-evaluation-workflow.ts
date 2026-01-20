9f7fb7bf53f4e5767baa996a942f6878e9da6fb5
Usman S. <ui.sabuwala@gmail.com>
2026-01-20 13:57:54 +0000

    add other steps to the workflow

--- apps/data-service/src/workflows/destination-evaluation-workflow.ts
+++ apps/data-service/src/workflows/destination-evaluation-workflow.ts
@@ -1,12 +1,47 @@
 import { WorkflowEntrypoint, WorkflowEvent, WorkflowStep } from 'cloudflare:workers';
 import { collectDestinationInfo } from '@/helpers/browser-render';
+import { aiDestinationChecker } from '@/helpers/ai-destination-checker';
+import { addEvaluation } from '@repo/data-ops/queries/evaluations';
+import { initDatabase } from '@repo/data-ops/database';
 
 export class DestinationEvaluationWorkflow extends WorkflowEntrypoint<Env, DestinationStatusEvaluationParams> {
 	async run(event: Readonly<WorkflowEvent<DestinationStatusEvaluationParams>>, step: WorkflowStep) {
+		initDatabase(this.env.DB);
+
 		const collectedData = await step.do('Collect rendered destination page data', async () => {
 			return collectDestinationInfo(this.env, event.payload.destinationUrl);
 		});
 
-		console.log(collectedData);
+		const aiStatus = await step.do(
+			'Use AI to check the page status',
+			{
+				retries: {
+					limit: 0,
+					delay: 0,
+				},
+			},
+			async () => {
+				return aiDestinationChecker(this.env, collectedData.bodyText);
+			}
+		);
+
+		const evaluationId = await step.do('Save evaluation in database', async () => {
+			return addEvaluation({
+				linkId: event.payload.linkId,
+				status: aiStatus.status,
+				reason: aiStatus.statusReason,
+				accountId: event.payload.accountId,
+				destinationUrl: event.payload.destinationUrl,
+			});
+		});
+
+		await step.do('Backup destination HTML to R2', async () => {
+			const accountId = event.payload.accountId;
+			const r2PathHtml = `evaluations/${accountId}/html/${evaluationId}`;
+			const r2PathBodyText = `evaluations/${accountId}/body-text/${evaluationId}`;
+
+			await this.env.BUCKET.put(r2PathHtml, collectedData.html);
+			await this.env.BUCKET.put(r2PathBodyText, collectedData.bodyText);
+		});
 	}
 }
