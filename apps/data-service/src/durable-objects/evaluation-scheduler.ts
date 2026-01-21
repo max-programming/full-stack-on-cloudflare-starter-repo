7d18d10299bba0d2c59a69b8598e1b886693bfe5
Usman S. <ui.sabuwala@gmail.com>
2026-01-21 10:34:35 +0000

    add a durable object for the workflow

---
+++ apps/data-service/src/durable-objects/evaluation-scheduler.ts
@@ -0,0 +1,52 @@
+import { DurableObject } from 'cloudflare:workers';
+import moment from 'moment';
+
+interface ClickData {
+	accountId: string;
+	linkId: string;
+	destinationUrl: string;
+	destinationCountryCode: string;
+}
+
+export class EvaluationScheduler extends DurableObject<Env> {
+	clickData: ClickData | undefined;
+
+	constructor(ctx: DurableObjectState, env: Env) {
+		super(ctx, env);
+		ctx.blockConcurrencyWhile(async () => {
+			this.clickData = await ctx.storage.get<ClickData>('clickData');
+		});
+	}
+
+	async collectLinkClick(accountId: string, linkId: string, destinationUrl: string, destinationCountryCode: string) {
+		this.clickData = {
+			accountId,
+			linkId,
+			destinationUrl,
+			destinationCountryCode,
+		};
+		await this.ctx.storage.put('clickData', this.clickData);
+
+		const alarm = await this.ctx.storage.getAlarm();
+		if (!alarm) {
+			const tenSeconds = moment().add(10, 'seconds').valueOf();
+			await this.ctx.storage.setAlarm(tenSeconds);
+		}
+	}
+
+	async alarm() {
+		console.log('Evaluation scheduler alarm triggered');
+		const clickData = this.clickData;
+		if (!clickData) {
+			throw new Error('No click data found');
+		}
+
+		await this.env.DESTINATION_EVALUATION_WORKFLOW.create({
+			params: {
+				linkId: clickData.linkId,
+				destinationUrl: clickData.destinationUrl,
+				accountId: clickData.accountId,
+			},
+		});
+	}
+}
--- apps/data-service/src/index.ts
+++ apps/data-service/src/index.ts
@@ -5,6 +5,7 @@ import { QueueMessageSchema } from '@repo/data-ops/zod-schema/queue';
 import { handleLinkClick } from './queue-handlers/link-clicks';
 
 export { DestinationEvaluationWorkflow } from './workflows/destination-evaluation-workflow';
+export { EvaluationScheduler } from './durable-objects/evaluation-scheduler';
 
 export default class DataService extends WorkerEntrypoint<Env> {
 	constructor(ctx: ExecutionContext, env: Env) {
